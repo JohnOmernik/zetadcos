4a3e05afca4d6b6da7b38f6ebb593dce9c1d60eb
johnomernik <john@omernik.com>
2016-11-22 13:11:30 -0600

    Final instructions

---
+++ 8_update_hosts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo ""
+echo "Now that your shared services are installed and working, you need to update your hosts to use the new ldap service"
+echo "To do that, run:"
+echo ""
+echo "./host_ldap_config.sh HOSTNAME"
+echo ""
+echo "This will be need to be run for any new hosts as well"
+
